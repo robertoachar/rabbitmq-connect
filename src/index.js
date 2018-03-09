380c1008a2658dce3599cc2b49d41230e215dc06
Roberto Achar <robertoachar@gmail.com>
2018-03-09 20:16:38 -0300

    style: add airbnb style guide

--- src/index.js
+++ src/index.js
@@ -3,9 +3,7 @@ const winston = require('winston');
 const broker = require('./broker');
 const config = require('./config');
 
-for (const key in config) {
-  winston.info(`${key}: ${config[key]}`);
-}
+Object.keys(config).map((key) => winston.info(`${key}: ${config[key]}`));
 
 broker.start().catch((err) => {
   winston.error(err.message);
